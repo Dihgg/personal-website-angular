4611a1158160d635a8eca58b60a8a0c0022fc504
Diego Lopes <dihgg@dihgg.com>
2021-08-09 15:13:03 -0300

    testing adjustments

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,7 +9,8 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ FooterComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   });
